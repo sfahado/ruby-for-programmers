ca9d9acd5a87e8b2d11e402c51887784fe2f5489
Fahad Idrees <fahad@sendoso.com>
2022-06-17 23:36:50 +0100

    Section#1 String

---
+++ string.rb
@@ -0,0 +1,36 @@
+# Ruby 
+#  Cross platform
+#  Object oriented
+
+# Ruby is an interpreted language so you can run it without first compiling it.
+
+# When a user interacts with a Raila's driven site, say, by writing a blog post or entering some data
+
+# into a wiki, then tools behind the scene process and embedded Ruby.
+
+# And from it they generate new HTML pages to be displayed in the browser.
+
+# Now, there are other ways that frameworks such as rails can be used, but I'm trying to give you a
+
+# typical example of the sort of ways that people create ruby powered websites.
+
+# Now, at the front end is the user interface of the application.
+
+# That's the actual Web pages.
+
+# And that's called The View.
+
+# The back end is the database and any operations required to retrieve or post new data items.
+
+# And that's called the model.
+
+# And between the two is the programming logic that cements the view and the model together.
+
+# And that's where most of the Ruby code lives.
+
+# And that's called the controller.
+
+# Now, put them all together and you've got an MVC model view controller framework.
+
+
+
